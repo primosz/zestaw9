bc67509cf73d5bb021312d8dc16f2fb0a1bbc18c
primosz <valadin96@gmail.com>
2017-12-12 12:35:55 +0100

    komit

---
+++ ConsoleApp1/HashSet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array1 = { 1, 2, 3 };
+        int[] array2 = { 3, 4, 5 };
+        int[] array3 = { 9, 10, 11 };
+
+        HashSet<int> set = new HashSet<int>(array1);
+        bool a = set.Overlaps(array2);
+        bool b = set.Overlaps(array3);
+
+        // Display results.
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
---
+++ ConsoleApp1/SortedSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Create list with elements.
+        List<string> list = new List<string>();
+        list.Add("sam");
+        list.Add("allen");
+        list.Add("perls");
+        list.Add("perls");
+
+        // Created sorted set from list.
+        SortedSet<string> set = new SortedSet<string>(list);
+
+        // Display contents.
+        foreach (string val in set)
+        {
+            Console.WriteLine(val);
+        }
+    }
+}
